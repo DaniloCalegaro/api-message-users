a5e5971d3562162009e82e2b33a0701f562d7025
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-10-10 17:28:03 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            notifications = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string nameProperty { get; set; }
+
+        [NotMapped]
+        public string message { get; set; }
+
+        [NotMapped]
+        public List<Notifies> notifications { get; set; }
+
+        public bool validatePropertyString(string value, string propertyName)
+        {
+            if(string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                notifications.Add(new Notifies
+                {
+                    message = "Campo Obrigatório",
+                    nameProperty = propertyName
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool validatePropertyInt(int value, string propertyName)
+        {
+            if (value <= 0 || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                notifications.Add(new Notifies
+                {
+                    message = "Campo Obrigatório",
+                    nameProperty = propertyName
+                });
+                return false;
+            }
+            return true;
+        }
+    }
+}
